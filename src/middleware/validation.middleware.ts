f51f1e0229ea6c9566c5fec9d3ac5dcf7c35b5cd
Nic Pearson <nic.s.pearson@gmail.com>
2021-03-06 20:59:33 +0100

    fix: rework validation middleware

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,22 +1,33 @@
 import Boom from 'boom';
-import { plainToClass } from 'class-transformer';
-import { ValidationError, validate } from 'class-validator';
-import { RequestHandler } from 'express';
+import { ClassType, transformAndValidate } from 'class-transformer-validator';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 
-function validationMiddleware<T>(type: T, skipMissingProperties = false): RequestHandler {
-  return (request, _, next) => {
-    validate(plainToClass(type as any, request.body), { skipMissingProperties }).then(
-      (errors: ValidationError[]) => {
-        if (errors.length > 0) {
-          const message = errors
-            .map((error: ValidationError) => Object.values(error.constraints!))
-            .join(';');
-          next(Boom.badRequest(message));
-        } else {
-          next();
-        }
+/**
+ * Creates middleware to validate a request, it triggers a BadRequest error
+ * if the request does not pass the given validation.
+ *
+ * @param dto the dto to validate against.
+ * @param message the error message to throw.
+ * @param location to location of the object.
+ */
+function validationMiddleware(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  dto: ClassType<any>,
+  message: string,
+  location: 'body' | 'params' | 'query' = 'body'
+): RequestHandler {
+  return async (req: Request, _: Response, next: NextFunction) => {
+    try {
+      const result = await transformAndValidate(dto, req[location], {
+        validator: { whitelist: true },
+      });
+      if (location === 'body') {
+        req.body = result;
       }
-    );
+      next();
+    } catch (error) {
+      next(Boom.badRequest(message));
+    }
   };
 }
 
