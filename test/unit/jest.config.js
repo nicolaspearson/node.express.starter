a998bcaf6b70a3d9202d6aed84911f1b50b8f8f2
Nic Pearson <nic.s.pearson@gmail.com>
2021-03-07 15:47:41 +0100

    fix: make workflows dependendant

--- test/unit/jest.config.js
+++ test/unit/jest.config.js
@@ -3,9 +3,12 @@ process.env.JEST_SUITE_NAME = 'api unit tests';
 module.exports = Object.assign({}, require(`../../jest.config.js`), {
   collectCoverageFrom: [
     'src/**/*.ts',
-    '!src/common/dto/**/*.ts',
-    '!src/db/**/*.ts',
     '!src/**/*.controller.ts',
+    '!src/common/dto/**/*.ts',
+    '!src/common/interfaces/**/*.ts',
+    '!src/db/**/*.ts',
+    '!src/index.ts',
+    '!src/utils.ts',
   ],
   setupFiles: ['<rootDir>/test/unit/setup.ts'],
   testMatch: ['**/test/unit/**/*.spec.ts'],
